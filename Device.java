058751158548692b191b0949567fe68b3bed29bb
Omar Adel Hussein Badawy <102391009+OmarAdelBadawy0@users.noreply.github.com>
2023-11-21 02:55:15 +0200

    complete all program but some problem in semaohore

--- Device.java
+++ Device.java
@@ -1,19 +1,20 @@
-public class Device {
+public class Device extends Thread {
     
     private String name;
     private String type;
     private Router router;
     
-    public Device(String name, String type) {
+    public Device(String name, String type, Router router) {
         this.name = name;
         this.type = type;
+        this.router = router;
     }
     
     public void connect(Router router) {
         this.router = router;
     }
 
-    public String getName() {
+    public String get_Name() {
         return name;
     }
 
@@ -25,7 +26,7 @@ public class Device {
         return router;
     }
 
-    public void setName(String name) {
+    public void set_Name(String name) {
         this.name = name;
     }
 
@@ -33,9 +34,19 @@ public class Device {
         this.type = type;
     }
 
-    public void setRouter(Router router) {
-        this.router = router;
+
+    public void doSomthing() {
+        System.out.println("(" + name + ") (" + type + ") - login");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-    
+    public void start(){
+    router.Occupy(this);
+    doSomthing();
+    router.exit(this);
+    }
 }
--- Network.java
+++ Network.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
+
 public class Network {
-    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the max number of conected devices in same time: ");
+        int capacity = input.nextInt();
+        Router router = new Router(capacity);
+        
+        int deviceCount;
+        System.out.println("Enter the number of devices: ");
+        deviceCount = input.nextInt();
+        Device[] devices = new Device[deviceCount];
+
+        for (int i = 0; i < deviceCount; i++) {
+
+            System.out.println("Enter the type of device " + (i + 1) + ": ");
+            String type = input.next();
+            devices[i] = new Device("C" + (i + 1), type, router);
+        }
+        for (int i = 0; i < deviceCount; i++) {
+            devices[i].start();
+        }
+
+
+
+    }
 }
